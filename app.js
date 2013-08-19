131ae2a1ceba259f9b8f7dfd59a21e56bff5364d
Alexander Ose <alex@sourcemap.com>
2013-08-19 11:19:03 -0400

    add spooky

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/svg/png', svg.png);
 app.post('/svg/png', svg.png);
 
 http.createServer(app).listen(app.get('port'), function(){
